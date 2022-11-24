13d67e9879fd1a9b95e4fa9724cf21c7e16570bf
Johan Svensson <johan@exclude.se>
2022-11-24 21:00:00 +0100

    small php stuff

--- php/index.php
+++ php/index.php
@@ -1,8 +1,22 @@
 <?php
 //declare(strict_types=1);
 
+print($_SERVER['REQUEST_METHOD']);
+switch($_SERVER['REQUEST_METHOD']) {
+    case "GET":
+        break;
+    case "POST":
+        break;
+    case "PUT":
+        break;
+        case "DELETE":
+            break;
+    default:
+            break;
+}
 phpinfo();
 
-$mysqli = new mysqli("127.0.0.1","my_user","my_password","my_db");
+
+$mysqli = new mysqli("db","backend","changeme","backend");
 $c = new Mosquitto\Client;
 ?>
