48914cfdd79bbfd5584fa9fe50aedabcece7386c
Johan Svensson <johan@exclude.se>
2022-12-11 12:31:49 +0100

    SQL: Add init script

---
+++ mysql_script/backend.sql
@@ -0,0 +1,1 @@
+CREATE TABLE `backend`.`backend` (`id` INT NOT NULL , `book_name` VARCHAR(255) NOT NULL , `ISBN` VARCHAR(255) NOT NULL , `rating` FLOAT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB; 
